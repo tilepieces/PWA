0804df5967553aedff4e6837e7c316f03be76812
tilepieces <90853332+tilepieces@users.noreply.github.com>
2022-03-30 18:07:48 +0200

    version 0.1.3

--- APPFILES.js
+++ APPFILES.js
@@ -1,4 +1,4 @@
-const VERSION = "0.1.2"; // THIS MUST BE CHANGED HERE
+const VERSION = "0.1.3"; // THIS MUST BE CHANGED HERE
 const CACHEAPPBASE = "cacheApp";
 const CACHEAPP = "cacheApp" + VERSION;
 const appFiles = [
