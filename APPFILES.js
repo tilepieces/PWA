7461ff1c15da03951bff5784ef45da93256f7ab1
tilepieces <90853332+tilepieces@users.noreply.github.com>
2022-04-02 15:00:38 +0200

    version 0.1.6
    update core version to 0.1.6

--- APPFILES.js
+++ APPFILES.js
@@ -1,4 +1,4 @@
-const VERSION = "0.1.5"; // THIS MUST BE CHANGED HERE
+const VERSION = "0.1.6"; // THIS MUST BE CHANGED HERE
 const CACHEAPPBASE = "cacheApp";
 const CACHEAPP = "cacheApp" + VERSION;
 const appFiles = [
