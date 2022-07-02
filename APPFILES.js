16dba30089fb86d08acf14033effd5b135c55a8d
tilepieces <90853332+tilepieces@users.noreply.github.com>
2022-07-02 11:52:53 +0200

    version 0.1.19
    core updated to version 0.1.18

--- APPFILES.js
+++ APPFILES.js
@@ -1,4 +1,4 @@
-const VERSION = "0.1.18"; // THIS MUST BE CHANGED HERE
+const VERSION = "0.1.19"; // THIS MUST BE CHANGED HERE
 const CACHEAPPBASE = "cacheApp";
 const CACHEAPP = "cacheApp" + VERSION;
 const appFiles = [
